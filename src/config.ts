3c4333ff8686a8942239008c7f2c609a785aca0a
Dominik M <ddominik.mackiewicz@gmail.com>
2026-02-21 21:01:00 +0100

    update source

--- src/config.ts
+++ src/config.ts
@@ -28,9 +28,9 @@ export const NAME = "";
 export const LICENSE = "MIT licensed.";
 
 // will be used to identify your bluesky account, so that likes and comments can be shown on your posts
-export const BLUESKY_IDENTIFIER = "flo-bit.dev";
+export const BLUESKY_IDENTIFIER = "dominiklabs.com";
 
-export const SOURCE_LINK = "https://github.com/flo-bit/blog-template";
+export const SOURCE_LINK = "https://github.com/mcdominik/blog";
 
 // will be used to set the base color of the blog
 export const BASE_COLOR: BaseColor = "neutral";
